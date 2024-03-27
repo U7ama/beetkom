324a1310b968afe0d6f65cbf7450a0c2e989f1e8
Sania Malik <sania.malik147258@gmail.com>
2024-03-27 14:45:02 +0500

    now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,20 @@ import logoImg from "../assets/loginLogo.png";
 import userImg from "../assets/user.png";
 import TableBar from "./TableBar";
 import { useNavigate } from "react-router-dom";
+
 const Users = () => {
   const navigate = useNavigate();
+
   const handleLogout = () => {
-    navigate("/");
+    const logoutConfirmed = window.confirm("Are you sure you want to logout?");
+    if (logoutConfirmed) {
+      navigate("/");
+    }
   };
+
   return (
     <div className="w-full flex flex-col">
-      {/* <div className="flex items-center justify-center w-full py-4 text-white bg-gray-700">
-        <p>Can't get your store to work? Call 0300-xxxxxxx</p>
-      </div> */}
-      {/* ced9da */}
-      <div className="flex ">
+      <div className="flex">
         <div className="flex w-1/2 justify-start p-8 bg-[#f1fffe] m-4 rounded-lg">
           <img src={userImg} alt="user" />
           <div className="ml-6 mt-2">
